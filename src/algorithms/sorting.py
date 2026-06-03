eec9ae1d9cfbab4864c7d54d37601112606eeb8b
matheusvictorio <matheusvic2016@gmail.com>
2026-06-03 18:38:04 -0300

    fix quick sort

--- src/algorithms/sorting.py
+++ src/algorithms/sorting.py
@@ -1,37 +1,46 @@
-def _safe_key(func, item, default=float('inf')):
-    val = func(item)
-    return (1, default) if val is None else (0, val)
-
-
 def quick_sort(data, key_func, reverse=False):
-    """Quick Sort recursivo. Valores None vão para o final."""
+    """Quick Sort recursivo. Valores None sempre vão para o final."""
+    valid = [x for x in data if key_func(x) is not None]
+    nones = [x for x in data if key_func(x) is None]
+    sorted_valid = _quick_sort_impl(valid, key_func)
+    if reverse:
+        sorted_valid = sorted_valid[::-1]
+    return sorted_valid + nones
+
+
+def _quick_sort_impl(data, key_func):
     if len(data) <= 1:
         return data
-    pivot_val = _safe_key(key_func, data[len(data) // 2])
-    left = [x for x in data if _safe_key(key_func, x) < pivot_val]
-    middle = [x for x in data if _safe_key(key_func, x) == pivot_val]
-    right = [x for x in data if _safe_key(key_func, x) > pivot_val]
-    result = quick_sort(left, key_func) + middle + quick_sort(right, key_func)
-    return result[::-1] if reverse else result
+    pivot_val = key_func(data[len(data) // 2])
+    left = [x for x in data if key_func(x) < pivot_val]
+    middle = [x for x in data if key_func(x) == pivot_val]
+    right = [x for x in data if key_func(x) > pivot_val]
+    return _quick_sort_impl(left, key_func) + middle + _quick_sort_impl(right, key_func)
 
 
 def merge_sort(data, key_func, reverse=False):
-    """Merge Sort estável. Valores None vão para o final."""
+    """Merge Sort estável. Valores None sempre vão para o final."""
+    valid = [x for x in data if key_func(x) is not None]
+    nones = [x for x in data if key_func(x) is None]
+    sorted_valid = _merge_sort_impl(valid, key_func)
+    if reverse:
+        sorted_valid = sorted_valid[::-1]
+    return sorted_valid + nones
+
+
+def _merge_sort_impl(data, key_func):
     if len(data) <= 1:
         return data
     mid = len(data) // 2
-    left = merge_sort(data[:mid], key_func)
-    right = merge_sort(data[mid:], key_func)
-    result = _merge(left, right, key_func)
-    return result[::-1] if reverse else result
+    left = _merge_sort_impl(data[:mid], key_func)
+    right = _merge_sort_impl(data[mid:], key_func)
+    return _merge(left, right, key_func)
 
 
 def _merge(left, right, key_func):
     result, i, j = [], 0, 0
     while i < len(left) and j < len(right):
-        lk = _safe_key(key_func, left[i])
-        rk = _safe_key(key_func, right[j])
-        if lk <= rk:
+        if key_func(left[i]) <= key_func(right[j]):
             result.append(left[i])
             i += 1
         else:
